210afff715f542c6e7bedd5d42a14b620802d1d3
mbeloch <michal@timegarden.com>
2015-07-05 16:45:09 +0200

    facebook wip

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
     <title></title>
 </head>
 <body>
-	<a href="public/facebook.php">Facebook</a>
+	<a href="public/facebook-login.php">Facebook</a>
 <?php
-	
-
+	echo('necum');
 ?>
 </body>
 </html>
--- public/facebook.php
+++ public/facebook.php
@@ -7,8 +7,31 @@
 <body>
 	<a href="../index.php">Home</a>
 <?php
-	
+session_start();
+define('facebook-php-sdk-v4-4.0-dev', '/FB/src/Facebook/');
+require __DIR__ . '/FB/autoload.php';	
 
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+
+FacebookSession::setDefaultApplication('516525445096951', '4dc6b5d8acd4d327b9410cb57a4213f4');
+
+$helper = new FacebookRedirectLoginHelper($redirect_url);
+try {
+    $session = $helper->getSessionFromRedirect();
+} catch(FacebookRequestException $ex) {
+    // When Facebook returns an error
+} catch(\Exception $ex) {
+    // When validation fails or other local issues
+}
+if ($session) {
+  // Logged in.
+}
+
+$request = new FacebookRequest($session, 'GET', '/me');
+$response = $request->execute();
+$graphObject = $response->getGraphObject();
 ?>
 </body>
 </html>
