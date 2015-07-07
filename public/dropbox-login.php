cb21702712d4335f3510623ba0fc802f44b0cedb
mbeloch <michal@timegarden.com>
2015-07-07 19:29:25 +0200

    dropbox api wip

--- public/dropbox-login.php
+++ public/dropbox-login.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['Drobpox']) && $_SESSION['Drobpox'] == true) {
     {
         $appInfo = dbx\AppInfo::loadFromJsonFile("Dropbox/config.json");
         $clientIdentifier = "my-app/1.0";
-        $redirectUri = "http://localhost/sources/public/dropbox-login.php";
+        $redirectUri = "http://localhost/sources/public/dropbox.php";
         $csrfTokenStore = new dbx\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
         return new dbx\WebAuth($appInfo, $clientIdentifier, $redirectUri, $csrfTokenStore);
     }
@@ -27,37 +27,6 @@ if (isset($_SESSION['Drobpox']) && $_SESSION['Drobpox'] == true) {
 
     $authorizeUrl = getWebAuth()->start();
     header("Location: $authorizeUrl");
-    //$smrdis = new dbx\Client($accessToken, $userId, $urlState);
-    //$_SESSION['Drobpox_token'] = $smrdis->getAccessToken();
-
-    try {
-        list($accessToken, $userId, $urlState) = getWebAuth()->finish($_GET);
-        assert($urlState === null);  // Since we didn't pass anything in start()
-    } catch (dbx\WebAuthException_BadRequest $ex) {
-        error_log("/dropbox-auth-finish: bad request: " . $ex->getMessage());
-        // Respond with an HTTP 400 and display error page...
-    } catch (dbx\WebAuthException_BadState $ex) {
-        // Auth session expired.  Restart the auth process.
-        header('Location: /dropbox-auth-start');
-    } catch (dbx\WebAuthException_Csrf $ex) {
-        error_log("/dropbox-auth-finish: CSRF mismatch: " . $ex->getMessage());
-        // Respond with HTTP 403 and display error page...
-    } catch (dbx\WebAuthException_NotApproved $ex) {
-        error_log("/dropbox-auth-finish: not approved: " . $ex->getMessage());
-    } catch (dbx\WebAuthException_Provider $ex) {
-        error_log("/dropbox-auth-finish: error redirect from Dropbox: " . $ex->getMessage());
-    } catch (dbx\Exception $ex) {
-        error_log("/dropbox-auth-finish: error communicating with Dropbox API: " . $ex->getMessage());
-    }
-
-    $_SESSION['Drobpox'] = true;
-    //$zmrd = getWebAuth()->start();
-
-    $client = dbx\Client($accessToken, $userId, $urlState);
-    $_SESSION['Drobpox_token'] = $_SESSION['dropbox-auth-csrf-token'];
-
-//    $zmrd = new dbx\AppInfo();
-//    $_SESSION['Drobpox_token'] = $zmrd;
 
 }
 
--- public/dropbox.php
+++ public/dropbox.php
@@ -12,22 +12,50 @@ session_start();
 require_once "Dropbox/autoload.php";
 use \Dropbox as dbx;
 
+if (!(isset($_SESSION['Drobpox']) && $_SESSION['Drobpox'] == true)) {
+    function getWebAuth()
+    {
+        $appInfo = dbx\AppInfo::loadFromJsonFile("Dropbox/config.json");
+        $clientIdentifier = "my-app/1.0";
+        $redirectUri = "http://localhost/sources/public/dropbox.php";
+        $csrfTokenStore = new dbx\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
+        return new dbx\WebAuth($appInfo, $clientIdentifier, $redirectUri, $csrfTokenStore);
+    }
 
-//$client = dbx\Client($accessToken);
-
-//echo dbx\appInfo::getKey();
-
-if (isset($_SESSION['Drobpox']) && $_SESSION['Drobpox'] == true){
-    echo "necum";
-    print_r($_SESSION['Drobpox_token']);
+    try {
+        list($accessToken, $userId, $urlState) = getWebAuth()->finish($_GET);
+        assert($urlState === null);  // Since we didn't pass anything in start()
+    }
+    catch (dbx\WebAuthException_BadRequest $ex) {
+        error_log("/dropbox-auth-finish: bad request: " . $ex->getMessage());
+        // Respond with an HTTP 400 and display error page...
+    }
+    catch (dbx\WebAuthException_BadState $ex) {
+        // Auth session expired.  Restart the auth process.
+        header('Location: /dropbox-auth-start');
+    }
+    catch (dbx\WebAuthException_Csrf $ex) {
+        error_log("/dropbox-auth-finish: CSRF mismatch: " . $ex->getMessage());
+        // Respond with HTTP 403 and display error page...
+    }
+    catch (dbx\WebAuthException_NotApproved $ex) {
+        error_log("/dropbox-auth-finish: not approved: " . $ex->getMessage());
+    }
+    catch (dbx\WebAuthException_Provider $ex) {
+        error_log("/dropbox-auth-finish: error redirect from Dropbox: " . $ex->getMessage());
+    }
+    catch (dbx\Exception $ex) {
+        error_log("/dropbox-auth-finish: error communicating with Dropbox API: " . $ex->getMessage());
+    }
+    $_SESSION['Drobpox'] = true;
+// We can now use $accessToken to make API requests.
+    $client = new dbx\Client($accessToken, $userId, $urlState);
+    $_SESSION['token'] = $client;
+    $client->getAccessToken();
+}else {
+    print_r($_SESSION['token']);
 }
 
-function getMetadata($path)
-{
-    Path::checkArg("path", $path);
-
-    return $this->_getMetadata($path, array("list" => "false"));
-}
 
 ?>
 </body>
