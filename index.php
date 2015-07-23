947118ca726c62cba436c1cc59d189e1195258a8
mbeloch <michal@timegarden.com>
2015-07-23 18:02:52 +0200

    react ajax

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title></title>
+    <script src="https://fb.me/react-0.13.3.js"></script>
+    <script src="https://fb.me/JSXTransformer-0.13.3.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
 	<a href="public/facebook.php">Facebook</a><br/>
@@ -14,5 +17,8 @@
 <?php
 session_start();
 ?>
+<div id="content"></div>
+    <script src="public/js/ajax-test.js" type="text/jsx"></script>
+
 </body>
 </html>
